d23902229e366b709016c76be72e3ccac82577d8
Hoongfei <841192493@qq.vom>
2021-11-20 18:46:00 +0800

    1

--- src/main/java/com/hf/controller/ConfigController.java
+++ src/main/java/com/hf/controller/ConfigController.java
@@ -1,5 +1,7 @@
 package com.hf.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,19 @@ public class ConfigController {
     private boolean useLocalCache;
 
     @RequestMapping("/get")
-    public boolean get() {
-        return useLocalCache;
+    @SentinelResource(value = "get", blockHandler = "deal_testHotKey", fallback = "test1")
+    public String get(Integer a) {
+        if (a == 2) {
+            throw new RuntimeException("1");
+        }
+        return "1";
+    }
+
+    public String deal_testHotKey(BlockException e) {
+        return "------deal_testHotKey,o(╥﹏╥)o" + e.getLocalizedMessage();  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
+    }
+
+    public String test1() {
+        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：fallback by Sentinel (flow limiting)
     }
 }
